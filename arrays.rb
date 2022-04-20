5471ecfd25e9363b498afda9abc765604361cc34
Gauri Joshi <gauri728@gmail.com>
2022-04-20 09:12:33 -0700

    Add self evaluation

--- arrays.rb
+++ arrays.rb
@@ -10,3 +10,11 @@ miles.unshift(11.7)
 #This will add the float 111.7 to the beginning of the miles array
 accounts_paid.push(false)
 #This will add a new boolean value of false to the end of the accounts_paid array
+colors[0]
+#This will call the first index position of the colors array
+jerseys[2]
+#This will call the 3rd index position of the jerseys array
+accounts_paid[-1]
+#This will call the last index position/element of the accounts_paid array
+colors.take(2)
+#This will return the first 2 elements in the colors array
