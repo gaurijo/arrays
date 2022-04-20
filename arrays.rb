f2a37e97aee6eae2e36bfbdd4cab6ed14f4124d1
Gauri Joshi <gauri728@gmail.com>
2022-04-20 09:01:40 -0700

    Display index positions

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,11 @@ colors = ["blue", "green", "red", "purple"]
 jerseys = [42, 7, 21, 90]
 miles = [12.2, 8.4, 2.1, 8.6]
 accounts_paid = [true, false, true, false]
+colors.pop
+#This will pop off the first element of the colors array
+jerseys.shift
+#This will remove the first element and return the first element of the jerseys array
+miles.unshift(11.7)
+#This will add the float 111.7 to the beginning of the miles array
+accounts_paid.push(false)
+#This will add a new boolean value of false to the end of the accounts_paid array
